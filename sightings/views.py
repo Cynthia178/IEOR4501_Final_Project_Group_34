cb36b669fa0e9f0894a155caeb15a0c5be73137e
Cynthia178 <81465498+Cynthia178@users.noreply.github.com>
2021-04-13 19:55:21 +0800

    Update views.py

--- sightings/views.py
+++ sightings/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Sighting
 
 def base_view(request):
-    return render(request,'sighting/base.html', {})
+    return render(request,'sightings/base.html', {})
+
+def map(request):
+    sightings = Sighting.objects.all()[:100]
+    context = {
+            'sightings':sightings,
+            }
+    return render(request,'sightings/map.html',context)
+
 
 
 
